d709ddf7d822849eabc8d2f4fa177fea0f79b39d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2025-04-22 14:35:49 +0200

    f

--- notebooks/vol-targeting.py
+++ notebooks/vol-targeting.py
@@ -1,7 +1,8 @@
-#%%
+# %%
 import yfinance as yf
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 sp500 = yf.Ticker("SPY")
 sp500_data = sp500.history(period="max")
@@ -9,18 +10,17 @@ sp500_data = sp500.history(period="max")
 sp500_data["Forward Return"] = sp500_data["Close"].pct_change(periods=1).shift(-1)
 
 # plot the equity curve
-import matplotlib.pyplot as plt
 
 plt.plot(sp500_data["Close"])
 plt.show()
 
-#%%
+# %%
 rolling_std = sp500_data["Close"].pct_change().rolling(20).std()
 plt.plot(rolling_std)
 plt.show()
 
 
-#%% measure the velocity of the volatility
+# %% measure the velocity of the volatility
 sp500_data["Vol Velocity"] = rolling_std.pct_change(30)
 plt.plot(sp500_data["Vol Velocity"])
 plt.show()
@@ -44,7 +44,9 @@ plt.show()
 
 
 # %%
-sns.barplot(x=sp500_data["Forward Return"], y=pd.cut(sp500_data["Vol Velocity"], bins=5))
+sns.barplot(
+    x=sp500_data["Forward Return"], y=pd.cut(sp500_data["Vol Velocity"], bins=5)
+)
 plt.show()
 
 # %%
