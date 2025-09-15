61417d5789bb2464fa00c6ac69491bd3d286c2b4
Daniel Szemerey <account@szemerey.eu>
2025-09-15 17:49:34 +0200

    f

---
+++ src/unravel_client/portfolio/universe.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import requests
+
+from ..constants import BASEAPI
+
+
+def get_historical_universe(
+    size: str, start_date: str, end_date: str, API_KEY: str
+) -> pd.DataFrame:
+    """
+    Fetch the historical universe from the Unravel API.
+
+    Args:
+        size (str): The universe size to use for the request. Pass in 'full' to get all available tickers for the portfolio.
+        start_date (str): The start date to use for the request.
+        end_date (str): The end date to use for the request.
+        API_KEY (str): The API key to use for the request
+
+    Returns:
+        pd.DataFrame: DataFrame of tickers in the portfolio [True and False]
+    """
+
+    url = f"{BASEAPI}/portfolio/universe"
+    params = {"size": size, "start_date": start_date, "end_date": end_date}
+
+    headers = {"X-API-KEY": API_KEY}
+    response = requests.get(url, headers=headers, params=params)
+    assert (
+        response.status_code == 200
+    ), f"Error fetching rolling universe for {size}, response: {response.json()}"
+
+    response = response.json()
+    return pd.DataFrame(
+        response["data"],
+        index=pd.to_datetime(response["index"]),
+        columns=response["columns"],
+    ).notna()
